1ac1ce5d5a13e35c128ee0a54771a2e453433e68
Продавец03 <apxujiy3@yandex.ru>
2022-05-10 18:11:31 +0200

    1345

--- main_script.py
+++ main_script.py
@@ -1,7 +1,12 @@
 from random import randint, choice, choices
 import json
+import sqlite3
 from tkinter import *
+from tkinter.ttk import Combobox
 
+window = Tk()
+window.title("Рандомайзер событий ролки")
+window.geometry('600x400')
 
 def warehouse_insignificant_event():
     pass
@@ -19,12 +24,27 @@ def significant_event():
     pass
 
 
+def startbottom():
+    global window
+    combo = Combobox(window)
+    combo['values'] = ('пустыня',
+                       'город-улей',
+                       'имперский городок',
+                       'имперские руины',
+                       'тундра',
+                       "ледяная пустошь")
+    combo.current(0)  # установите вариант по умолчанию
+    combo.grid(column=0, row=0)
+    return combo.current(), combo.get()
+
+
+def terrain_choise():
+    return print("123")
+
+
 def main_logic():
-    window = Tk()
-    window.title("Рандомайзер событий ролки")
-    window.geometry('600x400')
-    btn = Button(window, text="Не нажимать!")
-    btn.grid(column=1, row=0)
+    btn = Button(window, text="Старт", command=startbottom)
+    btn.grid(column=2, row=0)
     window.mainloop()
 
 
